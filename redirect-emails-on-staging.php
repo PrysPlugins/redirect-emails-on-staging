f2bb5d843f9271c4e7e9cf6d08f8eac7c36d8d72
Jeremy Pry <jeremy.pry@gmail.com>
2013-07-29 12:43:33 -0400

    Add the primary code

--- redirect-emails-on-staging.php
+++ redirect-emails-on-staging.php
@@ -13,3 +13,25 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	die( 'You can\'t do anything by accessing this file directly.' );
 }
+
+// We're using a high priority to give other plugins room to also modify this filter.
+add_filter( 'wp_mail', 'jpry_maybe_redirect_mail', 1000, 1 );
+
+/**
+ * Possibly filter all mail.
+ * 
+ * @uses is_wpe_snapshot() Checks to determine if this is a WP Engine Staging site.
+ *
+ * We only apply this filter if we're on the staging site, where we generally don't want email
+ * accidentally being sent out to end users.
+ * @param array $mail_args Array of settings for sending the message.
+ * @return array The args to use for the mail message
+ */
+function jpry_maybe_redirect_mail( $mail_args ) {
+	if ( function_exists( 'is_wpe_snapshot' ) && is_wpe_snapshot() ) {
+		$mail_args['message'] = 'Originally to: ' . $mail_args['to'] . "\n\n" . $mail_args['message'];
+		$mail_args['subject'] = 'Redirected mail | ' . $mail_args['subject'];
+		$mail_args['to'] = get_site_option( 'admin_email', 'jeremy@does.co' );
+	}
+	return $mail_args;
+}
