3345b6397a6e2c66834ebe94320ae673fa66296e
JPry <JPry@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-01 15:38:58 +0000

    Initial plugin creation
    git-svn-id: http://plugins.svn.wordpress.org/redirect-emails-on-staging/trunk@749318 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,41 @@
+=== Redirect Emails on Staging ===
+Contributors: JPry
+Tags: email, wpengine
+Requires at least: 3.5.2
+Tested up to: 3.5.2
+Stable Tag: 1.0
+License: GPL2
+
+Redirect any emails that are sent by the staging site to the administrator of the site.
+
+== Description ==
+
+When using the [Staging area](http://support.wpengine.com/staging/) on WP Engine, you may want to prevent the staging version of your site from sending emails to your site users. This plugin redirects all emails to the site admin.
+
+== Installation ==
+
+Simple installation:
+
+1. Upload the `WPE-redirect-emails-on-staging` folder to your `/wp-content/plugins/` directory
+2. Activate the plugin through the "Plugins" menu in WordPress
+
+Advanced installation (`mu-plugins` directory):
+
+1. Upload *just* the `redirect-emails-on-staging.php` file to your `/wp-content/mu-plugins/` directory
+2. That's it... the plugin is already activated! Take a look under Plugins > Must Use
+
+== Frequently Asked Questions ==
+
+None so far. Ask away!
+
+== Screenshots ==
+
+(none yet)
+
+== Changelog ==
+
+= 1.0 =
+* Created the plugin
+
+== Upgrade Notice ==
+
---
+++ redirect-emails-on-staging.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Plugin Name: Redirect Emails on Staging
+ * Plugin URI: https://github.com/JCPry/WPE-redirect-emails-on-staging
+ * Description: (For WP Engine only) On the Staging site, redirect all emails to the site admin. This is useful in making sure that the staging site doesn't send out confusing emails to your users.
+ * Version: 1.0
+ * Author: Jeremy Pry
+ * Author URI: http://jeremypry.com/
+ * License: GPL2
+ */
+
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You can\'t do anything by accessing this file directly.' );
+}
+
+// We're using a high priority to give other plugins room to also modify this filter.
+add_filter( 'wp_mail', 'jpry_maybe_redirect_mail', 1000, 1 );
+
+/**
+ * Possibly filter all mail.
+ *
+ * We only apply this filter if we're on the staging site, where we generally don't want email
+ * accidentally being sent out to end users.
+ * 
+ * @uses is_wpe_snapshot() Checks to determine if this is a WP Engine Staging site.
+ * 
+ * @param array $mail_args Array of settings for sending the message.
+ * @return array The args to use for the mail message
+ */
+function jpry_maybe_redirect_mail( $mail_args ) {
+	if ( function_exists( 'is_wpe_snapshot' ) && is_wpe_snapshot() ) {
+		$admin_email = get_site_option( 'admin_email' );
+		
+		// Only redirect email that is NOT already going to the admin
+		if ( $admin_email != $mail_args['to'] ) {
+			$mail_args['message'] = 'Originally to: ' . $mail_args['to'] . "\n\n" . $mail_args['message'];
+			$mail_args['subject'] = 'REDIRECTED MAIL | ' . $mail_args['subject'];
+			$mail_args['to'] = $admin_email;
+		}
+	}
+	return $mail_args;
+}
